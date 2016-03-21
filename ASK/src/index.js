ca29753c3e2cc7f66a146131758d6fa2f9e552e6
Jason Pilz <jasonpilz@gmail.com>
2016-03-20 18:46:09 -0600

    add method for calling the particle API

--- ASK/src/index.js
+++ ASK/src/index.js
@@ -1,4 +1,4 @@
-var http        = require('https');
+var https       = require('https');
 var AlexaSkill  = require('./AlexaSkill');
 
 var APP_ID      = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]"
@@ -35,10 +35,17 @@ Particle.prototype.eventHandlers.onSessionEnded = function(sessionEndedRequest, 
   // Handle Intents
 Particle.prototype.intentHandlers = {
   GetVoltageIntent: function(intent, session, response) {
-    var voltagePath = hostUrl + deviceUrl + "/voltage";
+    var voltagePath = deviceUrl + deviceId + "/voltage";
 
     console.log("Path: " + voltagePath);
 
+    callParticleApi(voltagePath, "", function(apiResponse) {
+      var parsedResponse = JSON.parse(apiResponse);
+      console.log("Voltage: " + parsedResponse.result);
+      resonse.tellWithCard("Voltage is " + parsedResponse.result + "volts",
+                           "Particle - Voltage",
+                           parsedResponse.result + " Volts");
+    });
   },
 
   GetMaximumVoltageIntent: function(intent, session, response) {
@@ -51,3 +58,37 @@ Particle.prototype.intentHandlers = {
     response.ask("You can ask me what is the voltage or max voltage. You can also tell me to turn on or off the light");
   }
 };
+
+function callParticleApi(path, args, callback) {
+  var options = {
+    hostname: hostUrl,
+    port: 443,
+    path: path,
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + accessToken
+    }
+  };
+
+  var req = https.request(options, function(res) {
+    console.log("statusCode: ", + res.statusCode);
+    console.log("headers: ", + res.headers);
+
+    var body = "";
+    res.setEncoding('utf8');
+
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+      callback(body);
+    });
+  });
+  req.end();
+
+  req.on('error', function(e) {
+    console.error(e);
+  });
+
+}
